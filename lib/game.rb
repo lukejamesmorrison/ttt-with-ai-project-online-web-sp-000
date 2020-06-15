0294e20b9b4ee42d31aa853d66f6f410701e5306
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 18:45:50 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
 
   attr_accessor :player_1, :player_2, :board
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
@@ -10,23 +10,24 @@ class Game
 
   def play
     @board.display
-    
-    turn while !over?
+
+    turn until over?
     puts won? ? "Congratulations #{winner}!" : "Cat's Game!"
   end
 
   def turn
-      position = current_player.move(@board)
+    position = current_player.move(@board)
 
-      if @board.valid_move?(position)
-        index = @board.position_as_index(position)
-        @board.cells[index] = current_player.token
-      else
-        puts "Oops, that position is taken. Try again."
-        turn
-      end
+    if @board.valid_move?(position)
+      index = @board.position_as_index(position)
+      @board.cells[index] = current_player.token
+    else
+      puts 'Oops, that position is taken. Try again.'
+      turn
+    end
 
-      @board.display
+    @board.display
+    turn
   end
 
   def current_player
@@ -62,7 +63,7 @@ class Game
       end
     end
 
-    winning_combo != nil ? winning_combo : false
+    winning_combo.nil? ? winning_combo : false
   end
 
   def draw?
@@ -76,7 +77,7 @@ class Game
   def winner
     if won?
       winning_token = @board.cells[won?[0]]
-      winning_player = player_1.token == winning_token ? player_1.token : player_2.token
+      player_1.token == winning_token ? player_1.token : player_2.token
     end
   end
 
@@ -84,15 +85,15 @@ class Game
   # The winning combinations of board cells.
   ##
   WIN_COMBINATIONS = [
-    [0,1,2], # Top row
-    [3,4,5],  # Middle row
-    [6,7,8],  # Bottom row
+    [0, 1, 2], # Top row
+    [3, 4, 5],  # Middle row
+    [6, 7, 8],  # Bottom row
 
-    [0,3,6],  # Left column
-    [1,4,7],  # Middle column
-    [2,5,8],  # Right column
+    [0, 3, 6],  # Left column
+    [1, 4, 7],  # Middle column
+    [2, 5, 8],  # Right column
 
-    [0,4,8],  # Top-Left cross
-    [6,4,2]  # Bottom-Left cross
+    [0, 4, 8],  # Top-Left cross
+    [6, 4, 2]  # Bottom-Left cross
   ]
 end
