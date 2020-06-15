fb1e32e632b3a0cea5fcfa6d0b4b2804af10f123
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 19:26:32 +0000

    Automatically backed up by Learn

--- spec/04_game_spec.rb
+++ spec/04_game_spec.rb
@@ -331,7 +331,7 @@ describe 'Game' do
     it 'plays through an entire game' do
       game = Game.new
       allow($stdout).to receive(:puts)
-      allow($stdin).to receive(:gets).and_return('foo')
+      allow($stdin).to receive(:gets).and_return('n')
 
       expect(game.player_1).to receive(:gets).and_return("1")
       expect(game.player_2).to receive(:gets).and_return("2")
