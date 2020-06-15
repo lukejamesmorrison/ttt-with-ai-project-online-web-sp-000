41e20e5a040ba1555a603a84f36295a13d81bc52
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 19:51:39 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -109,7 +109,6 @@ class Game
     if @board.valid_move?(position)
       index = @board.position_as_index(position)
       update(index, current_player)
-      # @board.cells[index] = current_player.token
     else
       puts 'Oops, that position is taken. Try again.'
       turn
