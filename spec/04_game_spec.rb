9f3225904087437c971dbc52edfcfbc5ecf8f8b5
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 19:27:21 +0000

    Automatically backed up by Learn

--- spec/04_game_spec.rb
+++ spec/04_game_spec.rb
@@ -331,7 +331,6 @@ describe 'Game' do
     it 'plays through an entire game' do
       game = Game.new
       allow($stdout).to receive(:puts)
-      allow($stdin).to receive(:gets).and_return('n')
 
       expect(game.player_1).to receive(:gets).and_return("1")
       expect(game.player_2).to receive(:gets).and_return("2")
@@ -342,8 +341,6 @@ describe 'Game' do
       expect(game.player_1).to receive(:gets).and_return("7")
 
       expect($stdout).to receive(:puts).with("Congratulations X!")
-
-      expect($stdin).to receive(:gets).and_return('n')
 
       game.play
     end
