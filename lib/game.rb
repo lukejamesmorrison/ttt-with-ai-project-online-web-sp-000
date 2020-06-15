476de9abcd0aca4ec8f975cb8a32179b363d4792
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 19:15:45 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -93,7 +93,7 @@ class Game
 
     # Play again?
     puts "Play again? [y/n]"
-    input = gets.strip
+    input = $stdin.gets.strip
     if ['y'].include?(input.downcase)
       Game.start
     end
