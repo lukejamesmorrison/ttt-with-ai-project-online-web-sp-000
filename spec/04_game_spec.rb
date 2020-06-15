af6c44d90d0569c57273279ac956fc039d7f55dc
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-15 19:19:13 +0000

    Automatically backed up by Learn

--- spec/04_game_spec.rb
+++ spec/04_game_spec.rb
@@ -342,7 +342,7 @@ describe 'Game' do
 
       expect($stdout).to receive(:puts).with("Congratulations X!")
 
-      expect($stdin).to receive(:gets).and_return('n')
+      expect(game).to receive(:gets).and_return('n')
 
       game.play
     end
